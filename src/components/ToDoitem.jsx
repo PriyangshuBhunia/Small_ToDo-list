3c1ec0c7624cabbfcf09d126cbbe9ec72915f2bc
Priyangshu Bhunia <priyangshubhunia03@gmail.com>
2024-07-21 21:49:13 +0530

    first commit

---
+++ src/components/ToDoitem.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function ToDoItem(props){
+    
+    // const[isDone, setIsDone] = useState(false);
+
+    // function handleClick(){
+    //     setIsDone((preValue)=> {
+    //         return !preValue; 
+    //     })
+    // }
+
+
+    // return(
+    //     <div onClick={handleClick}>
+    //          <li style={{textDecoration: isDone? 'line-through' : "none"}}>{props.text}</li>
+    //     </div>
+    // );
+    
+    
+    return(
+        <div onClick={()=>{
+            props.onChecked(props.id);
+        }}>
+            <li>{props.text}</li>
+        </div>
+    );
+    
+}
+
+export default ToDoItem;
